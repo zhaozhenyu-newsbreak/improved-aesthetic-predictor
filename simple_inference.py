3f252c38014af2ac3ae76e05420799727ed81c10
zhenyu.zhao <zhenyu.zhao@newsbreak.com>
2023-10-19 04:10:53 +0000

    u server

--- simple_inference.py
+++ simple_inference.py
@@ -1,85 +1,19 @@
-import webdataset as wds
 from PIL import Image
 import io
-import matplotlib.pyplot as plt
 import os
 import json
+import requests
 
-from warnings import filterwarnings
-
-
-# os.environ["CUDA_VISIBLE_DEVICES"] = "0"    # choose GPU if you are on a multi GPU server
-import numpy as np
 import torch
 import pytorch_lightning as pl
 import torch.nn as nn
-from torchvision import datasets, transforms
-import tqdm
-
-from os.path import join
-from datasets import load_dataset
-import pandas as pd
-from torch.utils.data import Dataset, DataLoader
-import json
-
 import clip
-
-
-from PIL import Image, ImageFile
+from PIL import Image
+import aiohttp
 
 
 #####  This script will predict the aesthetic score for this image file:
 
-img_path = "test.jpg"
-
-
-
-
-
-# if you changed the MLP architecture during training, change it also here:
-class MLP(pl.LightningModule):
-    def __init__(self, input_size, xcol='emb', ycol='avg_rating'):
-        super().__init__()
-        self.input_size = input_size
-        self.xcol = xcol
-        self.ycol = ycol
-        self.layers = nn.Sequential(
-            nn.Linear(self.input_size, 1024),
-            #nn.ReLU(),
-            nn.Dropout(0.2),
-            nn.Linear(1024, 128),
-            #nn.ReLU(),
-            nn.Dropout(0.2),
-            nn.Linear(128, 64),
-            #nn.ReLU(),
-            nn.Dropout(0.1),
-
-            nn.Linear(64, 16),
-            #nn.ReLU(),
-
-            nn.Linear(16, 1)
-        )
-
-    def forward(self, x):
-        return self.layers(x)
-
-    def training_step(self, batch, batch_idx):
-            x = batch[self.xcol]
-            y = batch[self.ycol].reshape(-1, 1)
-            x_hat = self.layers(x)
-            loss = F.mse_loss(x_hat, y)
-            return loss
-    
-    def validation_step(self, batch, batch_idx):
-        x = batch[self.xcol]
-        y = batch[self.ycol].reshape(-1, 1)
-        x_hat = self.layers(x)
-        loss = F.mse_loss(x_hat, y)
-        return loss
-
-    def configure_optimizers(self):
-        optimizer = torch.optim.Adam(self.parameters(), lr=1e-3)
-        return optimizer
 
 def normalized(a, axis=-1, order=2):
     import numpy as np  # pylint: disable=import-outside-toplevel
@@ -89,34 +23,81 @@ def normalized(a, axis=-1, order=2):
     return a / np.expand_dims(l2, axis)
 
 
-model = MLP(768)  # CLIP embedding dim is 768 for CLIP ViT L 14
+class MLP(pl.LightningModule):
+    def __init__(self, input_size, xcol='emb', ycol='avg_rating'):
+        super().__init__()
+        self.input_size = input_size
+        self.xcol = xcol
+        self.ycol = ycol
+        self.layers = nn.Sequential(
+            nn.Linear(self.input_size, 1024),
+            nn.Dropout(0.2),
+            nn.Linear(1024, 128),
+            nn.Dropout(0.2),
+            nn.Linear(128, 64),
+            nn.Dropout(0.1),
+            nn.Linear(64, 16),
+            nn.Linear(16, 1)
+        )
 
-s = torch.load("sac+logos+ava1-l14-linearMSE.pth")   # load the model you trained previously or the model available in this repo
-
-model.load_state_dict(s)
-
-model.to("cuda")
-model.eval()
+    def forward(self, x):
+        return self.layers(x)
 
 
-device = "cuda" if torch.cuda.is_available() else "cpu"
-model2, preprocess = clip.load("ViT-L/14", device=device)  #RN50x64   
+class Predictor:
+    def __init__(self, model_path='./sac+logos+ava1-l14-linearMSE.pth'):
+        self.model = MLP(768)
+        self.device = "cuda" if torch.cuda.is_available() else "cpu"
+        self.model.load_state_dict(torch.load(model_path))
+        self.model.to(self.device)
+        self.model.eval()
+        self.clip_model, self.preprocess = clip.load("ViT-L/14", device=self.device) # RN50x64  
 
+    def predict_img_url(self, url, normalize=True):
+        pil_image = Image.open(io.BytesIO(requests.get(url).content))
+        return self.predict_img(pil_image, normalize=normalize)
+    
+    async def predict_img_url_async(self, url, normalize=True):
+        async with aiohttp.ClientSession() as session:
+            async with session.get(url) as response:
+                if response.status == 200:
+                    image_data = await response.read()
+                    pil_image = Image.open(io.BytesIO(image_data))
+                    return self.predict_img(pil_image, normalize=normalize)
+                else:
+                    # Handle error cases here, e.g., return an error code or raise an exception.
+                    return None
+    
+    def predict_img_local_path(self, img_path, normalize=True):
+        pil_image = Image.open(img_path)
+        return self.predict_img(pil_image, normalize=normalize)
 
-pil_image = Image.open(img_path)
+    def predict_img(self, pil_img, normalize=True):
+        image = self.preprocess(pil_img).unsqueeze(0).to(self.device)
+        with torch.no_grad():
+            image_features = self.clip_model.encode_image(image)
+        im_emb_arr = normalized(image_features.cpu().detach().numpy())
+        prediction = self.model(torch.from_numpy(im_emb_arr).to(self.device).type(torch.cuda.FloatTensor))
+        if normalize:
+            prediction = torch.sigmoid(prediction)
+        return prediction[0].item()
+    
 
-image = preprocess(pil_image).unsqueeze(0).to(device)
-
-
-
-with torch.no_grad():
-   image_features = model2.encode_image(image)
-
-im_emb_arr = normalized(image_features.cpu().detach().numpy() )
-
-prediction = model(torch.from_numpy(im_emb_arr).to(device).type(torch.cuda.FloatTensor))
-
-print( "Aesthetic score predicted by the model:")
-print( prediction )
-
+if __name__ == '__main__':
+    image_path = './image_1.jpeg'
+    predictor = Predictor()
+    # print(predictor.predict_img_local_path(image_path))
+    # image_path = './image_2.jpeg'
+    # print(predictor.predict_img_local_path(image_path))
+    # image_path = './image_3.jpeg'
+    # print(predictor.predict_img_local_path(image_path))
+    images = [
+        'https://lh5.googleusercontent.com/p/AF1QipMv3b_BhZhkE2U1qMzN8tRv6x0WagBNoOVtyhU=s0',
+        'https://lh5.googleusercontent.com/p/AF1QipPK4vnsiL81l8EN-9lMJ1Zc62l82UMJGh2vzR8n=s0',
+        'https://lh5.googleusercontent.com/p/AF1QipOT9tywcIYGcVS1w3eVurVGregRFo5RfZKpHyWq=s0',
+        'https://lh5.googleusercontent.com/p/AF1QipOAJdpbiLIlaQuzmQvfO4MJUE7tU5QZmCBWe5cA=s0',
+        'https://lh5.googleusercontent.com/p/AF1QipN1YXw0-IBRc6gkDH4DLsO2eHwHZVEVmHkPHOS0=s0'
+    ]
+    for img in images:
+        print(predictor.predict_img_url(img))
 
